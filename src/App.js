9f76673127316e8e76a87164cd780227c0202b5a
youmemonk <ukeym160@gmail.com>
2022-01-07 21:17:56 +0530

    Switch?

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Home from "./Home";
+import Contact from "./Contact";
+import About from "./About";
+import Projects from "./Projects";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Switch,
+  Link,
+} from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+        <li>
+          <Link to="/projects">Projects</Link>
+        </li>
+      </ul>
+
+      
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/about" element={<About />}></Route>
+          <Route exact path="/contact" element={<Contact />}></Route>
+          <Route exact path="/projects" element={<Projects />}></Route>
+        </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Projects.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Projects() {
+  return (
+    <div>
+      <h1>Projects Page Section 1</h1>
+    </div>
+  );
+}
+
+export default Projects;
